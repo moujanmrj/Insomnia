ade50c63048d707305a82a87f47812a0f32dc320
9831140 <9831140@ceit.local>
2020-06-08 20:30:24 +0430

    Jurl class was added and request's input fields

---
+++ src/Jurl.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Jurl {
+    public static void main(String[] args) {
+        if(args.length == 0)
+        {
+            Scanner sc = new Scanner(System.in);
+            args = sc.nextLine().split("\\s+");
+        }
+
+        for (int i = 1; i < args.length; i++) {
+            String arg = args[i];
+
+        }
+    }
+}
